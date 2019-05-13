c978643a0b9165e9bfc680405f46d5ef9796a9d0
Vargen18 <45463154+Vargen18@users.noreply.github.com>
2019-05-13 16:25:26 +0200

    Initial Commit

---
+++ src/iMat/iMat.java
@@ -0,0 +1,38 @@
+package iMat;
+
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
+
+public class iMat extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        IMatDataHandler dataHandler = IMatDataHandler.getInstance();
+        System.out.println(dataHandler.isFirstRun()); //Testade en backend-funktion
+
+        ResourceBundle bundle = java.util.ResourceBundle.getBundle("iMat/resources/iMat");
+
+        Parent root = FXMLLoader.load(getClass().getResource("iMat.fxml"), bundle);
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle(bundle.getString("application.name"));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
+
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module iMat {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires projectbackend;
+
+
+    opens iMat;
+}
