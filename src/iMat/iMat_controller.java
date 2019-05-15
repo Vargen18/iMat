3c89f8947d6e4b642ff095c4aedac9dc0d62c6c9
Adam Törnqvist <adamtornqvist23@gmail.com>
2019-05-15 14:25:38 +0200

    Ny controller klass

--- src/iMat/iMat_controller.java
+++ src/iMat/iMat_controller.java
@@ -1,4 +1,26 @@
 package iMat;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+import java.util.ResourceBundle;
+
 public class iMat_controller {
+
+
+
+    @FXML
+    private Button switchSceneButton;
+
+    @FXML
+    private void switchScene() throws Exception{
+
+        iMat.switchScene(switchSceneButton, "Favorites.fxml");
+    }
+
+
 }
