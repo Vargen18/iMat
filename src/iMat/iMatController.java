32a82441490fe121dfd506fd94434620854468ec
Adam Törnqvist <adamtornqvist23@gmail.com>
2019-05-17 14:15:43 +0200

    Glömt bort hur man gör startfönstret större från början men lagt in escapehatch för favoritsidan

--- src/iMat/iMatController.java
+++ src/iMat/iMatController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
 
 import java.util.ResourceBundle;
@@ -12,6 +13,9 @@ public class iMatController {
 
     @FXML
     private Button switchSceneButton;
+
+    @FXML
+    private ImageView escapehatch;
 
 
     // IMatDataHandler dataHandler = new IMatDataHandler(); Den har private access. Tror vi måste komma åt den här
